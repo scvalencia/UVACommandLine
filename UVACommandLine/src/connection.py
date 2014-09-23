9c787d37c8590cb8d1a41fe34487ce2b02e291bd
scvalencia <sc.valencia606@gmail.com>
2014-09-23 02:55:02 -0500

    Structural changes

---
+++ UVACommandLine/src/connection.py
@@ -0,0 +1,19 @@
+import apiServices
+import submit
+
+class uvaConnection(object):
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.validation = submit.check_login(username, password)
+        self.user_id = None
+        self.submissions = None
+        if self.validation:
+            self.user_id = apiServices.username_to_id(username)
+            self.submissions = apiServices.get_submissions(self.user_id)
+
+    def submit_problem(self, lang, problem_number, filename, mode):
+        if self.validation:
+            submit.submit(self.username, self.password, lang, problem_number, filename, mode)
+            self.submissions = apiServices.get_submissions(self.user_id)
